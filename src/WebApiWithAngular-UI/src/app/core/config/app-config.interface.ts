299047e9d3a80245298f55a7a69c9f13ed10574b
Manoj Kumar Sharma <manojexamai@gmail.com>
2026-03-04 09:35:30 +0530

    Corrected the Project folders

--- src/WebApiWithAngular-UI/src/app/core/config/app-config.interface.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Represents application-wide runtime configuration.
- * This is injected using Angular's Dependency Injection system.
- */
-export interface AppConfig {
-  apiBaseUrl: string;
-}
